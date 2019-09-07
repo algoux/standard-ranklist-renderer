a2361087e9c6cf62d4c4644f61d38baf78e915e6
bLue <tbdblue@gmail.com>
2019-09-07 19:19:26 +0800

    Initial commit

---
+++ src/Ranklist.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'Ranklist.less';
+
+interface State {
+
+}
+
+export default class Ranklist extends React.Component<any, State> {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return <div className="ranklist">
+
+    </div>
+  }
+}
